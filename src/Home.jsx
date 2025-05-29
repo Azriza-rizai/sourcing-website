681079ff1497a086a07ab53db350872192df8dbc
Azriza-rizai <azriza.rizai@gmail.com>
2025-05-29 23:51:25 +0200

    latest

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,26 +3,29 @@ import "../css/Home.css"; // Import the CSS file for styling
 function Home() {
   return (
   <> 
-      <section className="hero">
+    <section className="hero">
      <div className="hero-content">
-      <h2>Your Global Sourcing Partner</h2>
-      <p>From concept to delivery, we connect your business to the world.</p>
-      <a href="#contact" className="cta-btn">Get in Touch</a>
+      <h1>Your Global Sourcing Partner</h1>
+      <h3>From concept to delivery, we connect your business to the world.</h3>
+      <a href="/contact" className="cta-btn">Get in Touch</a>
     </div>
-     </section>
+    </section>
 
     <section className="services" id="services">
-      <div className="container">
-        <h2>Key Services</h2>
+        <h2 className="key">Key Services</h2>
         <div className="supplier">
           <h3 className="find">Supplier identification and verification</h3>
           <p>
             Identify and evaluate potential suppliers based on their
             credibility, reputation, and product offerings.
           </p>
-        </div>
+      </div>
         <div className="procurement">
-          <h3 className="sourcing">Product sourcing <br/> <br/>and procurement</h3>
+          <div className="sourcing">
+            <div className="not-too-wide">
+              <h3 className="sourcing-title">Product sourcing and procurement</h3>
+            </div>
+          </div>
           <p>
             Identify and source products from trusted suppliers based on your
             requirements, preferences, and budget.
@@ -52,7 +55,6 @@ function Home() {
             secure transport of goods from supplier to destination..
           </p>
         </div>
-      </div>
     </section>
   </>
   );
