4d021ead2fc70dd0f210ea6865e38590a3ae76bd
Azriza-rizai <azriza.rizai@gmail.com>
2025-06-01 12:07:24 +0200

    ysa

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,24 +13,26 @@ function Home() {
 
     <section className="services" id="services">
         <h2 className="key">Key Services</h2>
+
+
         <div className="supplier">
           <h3 className="find">Supplier identification and verification</h3>
           <p>
             Identify and evaluate potential suppliers based on their
             credibility, reputation, and product offerings.
           </p>
-      </div>
+        </div>
+
+
         <div className="procurement">
-          <div className="sourcing">
-            <div className="not-too-wide">
-              <h3 className="sourcing-title">Product sourcing and procurement</h3>
-            </div>
-          </div>
+          <h3 className="sourcing">Product sourcing and procurement</h3>
           <p>
             Identify and source products from trusted suppliers based on your
             requirements, preferences, and budget.
           </p>
         </div>
+
+
         <div className="quality">
           <h3 className="inspection">Quality control and inspection</h3>
           <p>
@@ -38,23 +40,26 @@ function Home() {
             to ensure they meet pre-defined standards.
           </p>
         </div>
-        <div
-          className="prices">
+
+        <div className="prices">
           <h3>Negotiation of prices and terms</h3>
           <p>
             Secure the best pricing, payment conditions, and delivery terms
             through strategic negotiation with suppliers.
           </p>
         </div>
+        
+        
         <div
-          className="logistics"
-        >
+          className="logistics">
           <h3>Logistics and shipping management</h3>
           <p>
             Coordinate shipping, customs, and delivery to ensure timely and
             secure transport of goods from supplier to destination..
           </p>
         </div>
+
+
     </section>
   </>
   );
